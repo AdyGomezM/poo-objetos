6a008d80d47d9316e51d0e93521a4fba1e1ca2ad
AdyGomezM <adyfabiolag@gmail.com>
2023-05-16 12:29:31 -0500

    Actualizando...

---
+++ Persona.cs
@@ -0,0 +1,23 @@
+﻿using Objetos.Clases;
+using System;
+
+namespace Objetos
+{
+    internal class Persona
+    {
+        internal int Edad;
+
+        public bool Nombre { get; internal set; }
+        public string Apellido { get; internal set; }
+
+        internal void ImprimirDatos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator Persona(persona v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using objeto.Clases;
+using System;
+
+namespace objeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Persona persona = new Persona();
+
+            persona.Nombre = "Josue yeriel";
+            persona.Apellido = "pech cupido";
+            persona.Edad = 34;
+            persona.Salario = 199999999999999;
+
+            persona.ImprimirDatos();
+
+            Console.WriteLine("--------------------------------------------------------------------------");
+
+            Persona persona2 = new Persona();
+
+            persona2.Nombre = "chan ady";
+            persona2.Apellido = "ku gomes";
+            persona2.Edad = 34;
+            persona2.Salario = 500;
+            persona2.ImprimirDatos();
+        }
+    }
+}
